1b08cb283ae0dfcbc7833e7e619c1eb34671194f
ozberkcetin <ozberkctn@gmail.com>
2018-08-26 22:35:11 +0300

    Immutable and lodash are added to chrome dev tools.

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ import { AppRegistry } from "react-native";
 import App from "./App";
 import codePush from "react-native-code-push";
 
+if (__DEV__) {
+  // Pretty-prints ImmutableJS data structures in the chrome console
+
+  // Disable spammy error messages when you're developing on a device
+  // and the computer's clock is out of sync.
+  // https://github.com/facebook/react-native/issues/1598
+  const installDevTools = require("immutable-devtools");
+  const Immutable = require("immutable");
+  installDevTools(Immutable);
+  console.ignoredYellowBox = ["jsSchedulingOverhead"];
+
+  // Keep a reference to React handy in the console to experiment with
+  // component APIs.
+  window.React = require("react-native");
+  window.Lodash = require("lodash");
+}
+
 AppRegistry.registerComponent("Boilerplate", () => codePush({})(App));
